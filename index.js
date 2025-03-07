3f1f80ba7c2a9b8f5794c7d261f8118900fa0773
notshimi <22shimishimi22@gmail.com>
2025-03-07 16:39:49 +0800

    ui build

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 const PORT = process.env.port || 3001;
 
 app.use(express.json());
-app.use(cors());
+/* app.use(cors()); */
 app.use(express.static("build"));
 app.use("/users", usersRouter);
 app.use("/api", loginRouter);
